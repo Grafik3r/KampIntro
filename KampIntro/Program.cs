041f53cef36080ba51fba1a80d6f5f52c8d5a516
Grafiker <Grafiker@DESKTOP-6HMG5IJ>
2022-08-16 23:15:39 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type safety - tip güvenliği
+            //Do not repeat yourself - kendini tekrarlama
+            //Değer tutucu - kategori etiketi aslında bir değer tutucudur. Alias takma isim.
+            
+            string kategoriEtiketi = "kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmısMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+
+            if (sistemeGirisYapmısMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+                        
+            Console.WriteLine(kategoriEtiketi);
+            
+            
+        }
+    }
+}
